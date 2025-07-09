db7d9039b14e6eca670e1706d9820562943ec442
Smriti Sinha <smiriti.sinha28@gmail.com>
2025-07-09 18:35:11 +0530

    Studied today

---
+++ 01_Basics/04_Comparisons.js
@@ -0,0 +1,24 @@
+// simple comparisons
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// string value comparison to number
+console.log("2" > 1);
+console.log("02" > 1);
+
+// null value comparison
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// undefined comparison
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === // for comparing datatypes
+
+console.log("2" === 2);
